53de03a61762ff9bce37da3d3dcded5375aafb3a
Inbasu <just.me.juncy@gmail.com>
2024-09-28 15:08:16 +0300

    modified:   currency.py 	modified:   tests/test_currency.py

--- currency.py
+++ currency.py
@@ -2,5 +2,5 @@ class Dollar:
     def __init__(self, amount: int) -> None:
         self.amount = amount
 
-    def times(self, multipier: int) -> None:
-        self.amount *= multipier
+    def times(self, multipier: int) -> "Dollar":
+        return Dollar(self.amount * multipier)
