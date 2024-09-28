f9c61cb939c67c65744617c5baeb9fd68beff99f
Inbasu <just.me.juncy@gmail.com>
2024-09-28 17:51:33 +0300

    modified:   currency.py 	modified:   tests/test_currency.py

--- tests/test_currency.py
+++ tests/test_currency.py
@@ -2,12 +2,9 @@ from currency import Dollar
 
 
 def test_times() -> None:
-    dollar = Dollar(5)
-    product = dollar.times(2)
-    assert product.amount == 10
-    product = dollar.times(3)
-    assert product.amount == 15
+    assert Dollar(5).times(2) == Dollar(10)
+    assert Dollar(5).times(3) == Dollar(15)
 
 
 def test_equality() -> None:
-    assert Dollar(5).equals(Dollar(5))
+    assert Dollar(5) == Dollar(5)
