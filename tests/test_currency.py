f39e56515b4830966eeec820059129dcacbc6baf
Inbasu <just.me.juncy@gmail.com>
2024-09-28 18:11:03 +0300

    modified:   currency.py 	modified:   tests/test_currency.py

--- tests/test_currency.py
+++ tests/test_currency.py
@@ -1,10 +1,19 @@
-from currency import Dollar
+from currency import Dollar, Frank
 
 
-def test_times() -> None:
+def test_dollar_times() -> None:
     assert Dollar(5).times(2) == Dollar(10)
     assert Dollar(5).times(3) == Dollar(15)
 
 
-def test_equality() -> None:
+def test_dollar_equality() -> None:
     assert Dollar(5) == Dollar(5)
+
+
+def test_frank_times() -> None:
+    assert Frank(5).times(2) == Frank(10)
+    assert Frank(5).times(3) == Frank(15)
+
+
+def test_frank_equality() -> None:
+    assert Frank(5) == Frank(5)
