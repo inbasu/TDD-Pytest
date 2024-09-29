294cc351659650c783e8aeb8ac77ae8cede479cd
Inbasu <just.me.juncy@gmail.com>
2024-09-29 14:35:06 +0300

    modified:   .pre-commit-config.yaml 	modified:   currency.py 	modified:   tests/test_currency.py

--- tests/test_currency.py
+++ tests/test_currency.py
@@ -1,9 +1,13 @@
-from currency import Bank, Expression, Money, Pair, Sum
+import pytest
+
+from currency import Bank, Money, Pair, Sum
 
 
-def test_dollar_times() -> None:
-    assert Money.dollar(5).times(2) == Money.dollar(10)
-    assert Money.dollar(5).times(3) == Money.dollar(15)
+@pytest.fixture
+def bank() -> Bank:
+    bank = Bank()
+    bank.add_rate("CHF", "USD", 2)
+    return bank
 
 
 def test_equality() -> None:
@@ -12,52 +16,56 @@ def test_equality() -> None:
     assert Money.frank(5) != Money.dollar(5)
 
 
-def test_frank_times() -> None:
+def test_money_times() -> None:
     assert Money.frank(5).times(2) == Money.frank(10)
-    assert Money.frank(5).times(3) == Money.frank(15)
+    assert Money.dollar(5).times(3) == Money.dollar(15)
 
 
 def test_currency() -> None:
-    """Property was disabeled in python"""
+    """No privat or protected prop in Python"""
     assert Money.frank(1).currency == "CHF"
     assert Money.dollar(1).currency == "USD"
 
 
-def test_simple_add() -> None:
-    sum: Expression = Money.dollar(5) + Money.dollar(5)
-    bank = Bank()
-    reducer = bank.reduce(sum, "USD")
+def test_simple_add(bank: Bank) -> None:
+    reducer = bank.reduce(Money.dollar(5) + Money.dollar(5), "USD")
     assert Money.dollar(10) == reducer
 
 
-def test_reduce_sum() -> None:
-    sum: Expression = Sum(Money.dollar(1), Money.dollar(1))
-    bank = Bank()
-    reducer = bank.reduce(sum, "USD")
-    assert Money.dollar(2) == reducer
-
-
-def test_reduce_money() -> None:
-    bank = Bank()
+def test_reduce_money(bank: Bank) -> None:
     reduced = bank.reduce(Money.dollar(1), "USD")
     assert reduced == Money.dollar(1)
 
 
-def test_add_rate_to_bank() -> None:
-    bank = Bank()
-    bank.add_rate("CHF", "USD", 2)
+def test_add_rate_to_bank(bank: Bank) -> None:
     assert bank.rates.get(Pair("CHF", "USD")) == 2
 
 
-def test_reduce_differnt_currency() -> None:
-    bank = Bank()
-    bank.add_rate("CHF", "USD", 2)
+def test_reduce_differnt_currency(bank: Bank) -> None:
     reduced = bank.reduce(Money.frank(2), "USD")
     assert reduced == Money.dollar(1)
 
 
-def test_mixed_currency_add() -> None:
-    bank = Bank()
-    bank.add_rate("CHF", "USD", 2)
+def test_mixed_currency_add(bank: Bank) -> None:
     reduced = bank.reduce(Money.dollar(5) + Money.frank(10), "USD")
     assert reduced == Money.dollar(10)
+
+
+def test_add_expressions(bank: Bank) -> None:
+    sum = Sum(Money.dollar(5), Money.frank(10))
+    reduced = bank.reduce(sum + Money.dollar(5), "USD")
+    assert reduced == Money.dollar(15)
+    reduced = bank.reduce(sum + Sum(Money.dollar(5), Money.frank(10)), "USD")
+    assert reduced == Money.dollar(20)
+
+
+def test_add_return_money(bank: Bank) -> None:
+    sum = Sum(Money.dollar(5), Money.frank(10))
+    reduced = bank.reduce(sum + Money.dollar(5), "USD")
+    assert isinstance(reduced, Money)
+
+
+def test_sum_times(bank: Bank) -> None:
+    sum = Sum(Money.dollar(5), Money.frank(10)).times(2)
+    reduced = bank.reduce(sum, "USD")
+    assert reduced == Money.dollar(20)
